f3d045c90bc2adc28763cbfc66d4ccdaabba907a
all0cer <italoalmeinasc@gmail.com>
2024-07-16 08:39:10 -0300

    rest controller pong

--- webhook-fluig-main/src/main/java/br/com/sebrae/uti/webhookfluig/controller/PingController.java
+++ webhook-fluig-main/src/main/java/br/com/sebrae/uti/webhookfluig/controller/PingController.java
@@ -1,10 +1,11 @@
 package br.com.sebrae.uti.webhookfluig.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PingController {
+
 	@GetMapping("/ping")
 	public String ping() {
 		return "pong";
