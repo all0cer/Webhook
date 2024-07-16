b3be48c1a198755c609d385f8ca9f23da4812da3
all0cer <italoalmeinasc@gmail.com>
2024-07-16 08:17:47 -0300

    ad controller test

---
+++ webhook-fluig-main/src/main/java/br/com/sebrae/uti/webhookfluig/controller/PingController.java
@@ -0,0 +1,12 @@
+package br.com.sebrae.uti.webhookfluig.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PingController {
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
+	}
+}
